98ea7fcd0487234b5002ff657eb68377e1f8d670
mansimahabdi23 <mansimahabdi@gmail.com>
2026-01-29 16:30:29 +0530

    with tampering

--- Assignment1/textfile.py
+++ Assignment1/textfile.py
@@ -23,14 +23,14 @@ class Block:
         }, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
     
-    
+
 class FileBlockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
         """Create the genesis block."""
-        return Block(0, time(), "0", "0")
+        return Block(0, time(), "Genesis block", "0")
     
     def get_latest_block(self):
         """Get the latest block in the chain."""
@@ -61,10 +61,10 @@ class FileBlockchain:
             previous = self.chain[i - 1]
 
             if current.hash != current.calculate_hash():
-                print(f"Alert: Block {current.index} has been tampered with!")
+                print(f"Alert: Block {current.index} has been tampered with")
                 return False
             if current.previous_hash != previous.hash:
-                print(f"Alert: Block {current.index} previous hash mismatch!")
+                print(f"Alert: Block {current.index} previous hash mismatch")
                 return False
         return True
 
@@ -72,7 +72,6 @@ class FileBlockchain:
 my_registry = FileBlockchain()
 
 # 2. Add a text file to the blockchain 
-# (Ensure 'contract.txt' exists in your directory first!)
 my_registry.add_file_block("contract.txt")
 
 # 3. View the Chain
@@ -80,3 +79,17 @@ for block in my_registry.chain:
     print(f"\nBlock {block.index} | Hash: {block.hash[:20]}...")
     print(f"File Hash: {block.file_hash[:20]}...")
     print(f"Prev Hash: {block.previous_hash[:20]}...")
+    # print(f"Timestamp:" {block.time})
+
+print(f"Is blockchain valid? {my_registry.is_chain_valid()}")
+
+print("\n--- Simulating the Tampering ---")
+print("\n--- After Tampering!! ---")
+
+my_registry.chain[1].file_hash = "fake_hash_value" 
+for block in my_registry.chain:
+    print(f"\nBlock {block.index} | Hash: {block.hash[:20]}...")
+    print(f"File Hash: {block.file_hash[:20]}...")
+    print(f"Prev Hash: {block.previous_hash[:20]}...")
+
+print(f"Is blockchain valid after tampering? {my_registry.is_chain_valid()}")
