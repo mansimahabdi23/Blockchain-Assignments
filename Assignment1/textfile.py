a0ef0612c04f9fdbf9a78ea33ab8d7ef11c7341a
mansimahabdi23 <mansimahabdi@gmail.com>
2026-02-05 14:32:24 +0530

    textfile with nonce

--- Assignment1/textfile.py
+++ Assignment1/textfile.py
@@ -20,14 +20,15 @@ class Block:
             "index" : self.index,
             "timestamp" : self.timestamp,
             "file_hash" : self.file_hash,
-            "previous_hash" : self.previous_hash
-            
+            "previous_hash" : self.previous_hash,
+            "nonce":self.nonce
         }, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
     
 
 class FileBlockchain:
-    def __init__(self):
+    def __init__(self, difficulty=4):
+        self.difficulty = difficulty
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
@@ -37,6 +38,14 @@ class FileBlockchain:
     def get_latest_block(self):
         """Get the latest block in the chain."""
         return self.chain[-1]
+    
+    def  mine_block(self, block:Block) -> bool:
+        target = "0" * self.difficulty
+        while not block.hash.startswith(target):
+            block.nonce += 1
+            block.hash = block.calculate_hash()
+        return block
+
     
     def add_file_block(self, file_path):
         """Reads a file, hashes it and adds a new block"""
@@ -52,12 +61,21 @@ class FileBlockchain:
                 previous_hash=self.get_latest_block().hash
             )
 
+            print(f"Mining Block #{new_block.index}...")
+            mined_block = self.mine_block(new_block)
+            self.chain.append(mined_block)
+
+            print(f"Success: Block added with Hash: {mined_block.hash[:20]}")
+            # print(f": {mined_block.hash[:20]}")
+            
+
             self.chain.append(new_block)
             print(f"Success: Block #{new_block.index} added for {file_path}")
         except FileNotFoundError:
             print(f"Error: File {file_path} not found.")
     
     def is_chain_valid(self):
+        target = "0" * self.difficulty
         for i in range(1, len(self.chain)):
             current = self.chain[i]
             previous = self.chain[i - 1]
@@ -71,16 +89,16 @@ class FileBlockchain:
         return True
 
 # 1. Initialize the Blockchain
-my_registry = FileBlockchain()
+my_registry = FileBlockchain(difficulty=4)
 
 # 2. Add a text file to the blockchain 
 my_registry.add_file_block("contract.txt")
 
 # 3. View the Chain
 for block in my_registry.chain:
-    print(f"\nBlock {block.index} | Hash: {block.hash[:20]}...")
-    print(f"File Hash: {block.file_hash[:20]}...")
-    print(f"Prev Hash: {block.previous_hash[:20]}...")
+    print(f"\nBlock {block.index} | Hash: {block.hash[:20]}")
+    print(f"File Hash: {block.file_hash[:20]}")
+    print(f"Prev Hash: {block.previous_hash[:20]}")
     # print(f"Timestamp:" {block.time})
 
 print(f"Is blockchain valid? {my_registry.is_chain_valid()}")
@@ -90,8 +108,9 @@ print("\n--- After Tampering!! ---")
 
 my_registry.chain[1].file_hash = "fake_hash_value" 
 for block in my_registry.chain:
-    print(f"\nBlock {block.index} | Hash: {block.hash[:20]}...")
-    print(f"File Hash: {block.file_hash[:20]}...")
-    print(f"Prev Hash: {block.previous_hash[:20]}...")
+    print(f"\nBlock {block.index} | Hash: {block.hash[:20]}")
+    print(f"File Hash: {block.file_hash[:20]}")
+    print(f"Prev Hash: {block.previous_hash[:20]}")
 
 print(f"Is blockchain valid after tampering? {my_registry.is_chain_valid()}")
+
